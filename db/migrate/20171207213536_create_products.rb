1ec361a334f12c592f122f46a107e77cdc753ba5
Bably Kumari <bably@Bablys-MacBook-Pro.local>
2017-12-09 17:20:28 -0800

    add index page

---
+++ db/migrate/20171207213536_create_products.rb
@@ -0,0 +1,9 @@
+class CreateProducts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :products do |t|
+    	t.column :name, :string
+    	t.column :price, :integer
+    	t.timestamp
+    end
+  end
+end
